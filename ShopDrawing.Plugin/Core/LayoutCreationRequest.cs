12cfc3bd396c6239f3fb446140c6397dbb80ff0c
huydaobk <huy.daobk@gmail.com>
2026-04-09 15:23:41 +0700

    fix(layout): include LayoutViewKind in shared request model

--- ShopDrawing.Plugin/Core/LayoutCreationRequest.cs
+++ ShopDrawing.Plugin/Core/LayoutCreationRequest.cs
@@ -1,7 +1,22 @@
+using System;
+using System.Collections.Generic;
 using Autodesk.AutoCAD.DatabaseServices;
 
 namespace ShopDrawing.Plugin.Core
 {
+    public enum LayoutViewKind
+    {
+        Elevation,
+        Plan
+    }
+
+    public sealed record LayoutViewRequest
+    {
+        public LayoutViewKind Kind { get; init; } = LayoutViewKind.Elevation;
+        public string UserTitle { get; init; } = "";
+        public Extents3d Region { get; init; }
+    }
+
     public sealed record LayoutCreationRequest
     {
         public string UserTitle { get; init; } = "";
@@ -13,5 +28,16 @@ namespace ShopDrawing.Plugin.Core
         public double MarginTop { get; init; } = 5;
         public double MarginBot { get; init; } = 5;
         public LayoutTitleBlockConfig TitleBlockConfig { get; init; } = new();
+        public IReadOnlyList<LayoutViewRequest> SecondaryViews { get; init; } = Array.Empty<LayoutViewRequest>();
+
+        public LayoutViewRequest CreatePrimaryView()
+        {
+            return new LayoutViewRequest
+            {
+                Kind = LayoutViewKind.Elevation,
+                UserTitle = UserTitle,
+                Region = Region
+            };
+        }
     }
 }

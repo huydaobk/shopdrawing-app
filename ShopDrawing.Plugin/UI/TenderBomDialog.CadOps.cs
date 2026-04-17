8a4612487e67d2cb409849c7da581129b653921b
huydaobk <huy.daobk@gmail.com>
2026-04-17 15:11:25 +0700

    fix(tender): free pick base points for opening height and bottom

--- ShopDrawing.Plugin/UI/TenderBomDialog.CadOps.cs
+++ ShopDrawing.Plugin/UI/TenderBomDialog.CadOps.cs
@@ -2389,8 +2389,7 @@ private void RepickWallFromCad(TenderWallRow targetRow, bool pickArea)
                 AllowNegative = false,
                 AllowZero = false,
                 UseDefaultValue = true,
-                UseBasePoint = true,
-                BasePoint = p1Result.Value
+                UseBasePoint = false
             };
             var hRes = ed.GetDistance(hOpt);
             if (hRes.Status != Autodesk.AutoCAD.EditorInput.PromptStatus.OK)
@@ -2398,14 +2397,13 @@ private void RepickWallFromCad(TenderWallRow targetRow, bool pickArea)
             double heightMm = Math.Round(hRes.Value);
             if (heightMm <= 0)
                 return false;
-            var bottomOpt = new Autodesk.AutoCAD.EditorInput.PromptDistanceOptions("\nNhập hoặc pick ĐIỂM THỨ 3 khoảng cách đáy (mm):")
+            var bottomOpt = new Autodesk.AutoCAD.EditorInput.PromptDistanceOptions("\nNhập hoặc pick ĐIỂM THỨ 4 khoảng cách đáy (mm):")
             {
                 DefaultValue = 0,
                 AllowNegative = false,
                 AllowZero = true,
                 UseDefaultValue = true,
-                UseBasePoint = true,
-                BasePoint = p1Result.Value
+                UseBasePoint = false
             };
             var bottomRes = ed.GetDistance(bottomOpt);
             if (bottomRes.Status != Autodesk.AutoCAD.EditorInput.PromptStatus.OK)
